ee16357ae50fdbd3d1810118af0d5ed05937dc73
Joe Koop <joe@joekoop.com>
2024-06-13 21:46:18 -0500

    rewrite to be generic

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
 # zig-deb
 
-Here are the scripts and helper files I use to create `.deb` files from precompiled binaries of [Zig], which I host on [my apt packages server][packagesJK].
+Here is the script I use to create `.deb` files from precompiled binaries of [Zig], which I host on [my apt packages server][packagesJK].
 
 I am not endorsed by [the Zig Software Foundation][zsf]. I just like their project, and see that they aren't publishing `.deb` files of their own.
 
 ## Usage
 
-**The easy, trusting way:**
-
 1. Go to http://packages.joekoop.com and run the two commands at the top of the page.
 1. Update your local indecies: `sudo apt update`
 1. Install Zig: `sudo apt install zig`
 
-**The slightly more difficult, less trusting way:**
-
-1. Clone this repo: `git clone https://github.com/jkoop/zig-deb.git`
-1. Change to the directory that corresponds with the version you want to package.
-1. (Trust check) Read `make-package.sh` and look for anything that you don't like.
-   - The script downloads the precompiled binaries and libraries from ziglang.org itself,
-   - checks their ligitimacy with [minisign],
-   - copies them into a new directory along with the metadata file, DEBIAN-control,
-   - and builds a `.deb` file with `dpkg-deb` ([man page][dpkg-deb]).
-1. Run the script: `./make-package.sh`
-1. The resulting `.deb` file will be in your CWD.
-
 [Zig]: https://ziglang.org/
 [packagesJK]: http://packages.joekoop.com/
 [zsf]: https://ziglang.org/zsf/
-[minisign]: https://jedisct1.github.io/minisign/
-[dpkg-deb]: https://man7.org/linux/man-pages/man1/dpkg-deb.1.html
---
+++ zig-deb.php
@@ -0,0 +1,220 @@
+#!/usr/bin/php8.1
+<?php
+
+/**
+ * Key: arch as called by Zig
+ * Value: arch as called by Debian packaging system
+ */
+const ARCH_MAP = [
+    // "aarch64" => "arm64",
+    // "i386" => "i386",
+    "x86_64" => "amd64",
+    // "x86" => "i386",
+];
+
+const WGET = "wget --quiet --show-progress -O 'tarball' "; // this must end with a space
+const COMPLETE_LIST_PATH = "complete.list";
+
+if (!file_exists(COMPLETE_LIST_PATH)) file_put_contents(COMPLETE_LIST_PATH, "");
+$complete = explode("\n", file_get_contents(COMPLETE_LIST_PATH));
+$complete = array_filter($complete, "strlen");
+
+$releases = json_decode(file_get_contents("https://ziglang.org/download/index.json"));
+
+foreach ($releases as $version => $files) {
+    if ($version == "master") continue;
+    foreach ($files as $arch => $props) {
+        $package_date = date("Y-m-d");
+
+        if (!str_ends_with($arch, "-linux")) {
+            // debug("Arch not linux: %s", $arch);
+            continue;
+        }
+
+        // remove "-linux" from the end
+        $arch = substr($arch, 0, strpos($arch, "-linux"));
+
+        if (!isset(ARCH_MAP[$arch])) {
+            warning("Arch not supported: %s", $arch);
+            continue;
+        }
+
+        $arch = ARCH_MAP[$arch]; // convert to Debiany name
+        $minor_version = substr($version, 0, strrpos($version, "."));
+        $package_name = "zig" . $minor_version;
+        $deb_file_name = $package_name . '_' . $version . '_' . $arch;
+
+        if (in_array($deb_file_name, $complete)) {
+            // info("Already packaged: %s", $deb_file_name);
+            continue;
+        }
+
+        passthru("rm -rf tarball tarball.d $deb_file_name $deb_file_name.deb");
+        printf("\n\tBuilding \"%s.deb\"\n\n", $deb_file_name);
+
+        $exit_code = -1;
+        info("Downloading tarball: %s", $props->tarball);
+        passthru(WGET . escapeshellarg($props->tarball), $exit_code);
+        if ($exit_code != 0) {
+            error("wget exit code non-zero: %d", $exit_code);
+            continue;
+        }
+
+        $exit_code = -1;
+        info("Extracting tarball", $props->tarball);
+        mkdir("tarball.d");
+        chdir("tarball.d");
+        passthru('tar xf ../tarball', $exit_code);
+        if ($exit_code != 0) {
+            error("tar exit code non-zero: %d", $exit_code);
+            continue;
+        }
+        chdir(__DIR__);
+
+        info("Staging package: %s", $deb_file_name);
+        mkdir($deb_file_name . '/opt/zig/' . $package_name, recursive: true);
+        mkdir($deb_file_name . '/usr/sbin', recursive: true);
+        chdir($deb_file_name);
+        passthru('cp -l ../tarball.d/*/zig opt/zig/' . $package_name);
+        passthru('cp -lr ../tarball.d/*/lib opt/zig/' . $package_name);
+        passthru('ln -s /opt/zig/' . $package_name . '/zig usr/sbin/' . $package_name);
+
+        $installed_size = trim(`du --block-size=1024 --summarize . | grep -Eo '^[0-9]*'`);
+        mkdir("DEBIAN");
+        file_put_contents("DEBIAN/control", <<<TXT
+        Package: $package_name
+        Version: $version
+        Architecture: $arch
+        Maintainer: Zig Software Foundation <https://ziglang.org/zsf>
+        Vendor: Joe Koop <https://github.com/jkoop>
+        Installed-Size: $installed_size
+        Section: devel
+        Priority: optional
+        Homepage: https://ziglang.org/
+        Description: A programming language for robust and efficient software
+         Zig[1] is a general-purpose programming language and toolchain for
+         maintaining robust, optimal and reusable software.
+         .
+         See also: the language docs[2], std docs[3], and release notes[4]
+         .
+         Built by ZSF on $files->date; packaged for Debian by Joe Koop[5] on $package_date
+         .
+         1: https://ziglang.org
+         2: $files->docs
+         3: $files->stdDocs
+         4: $files->notes
+         5: https://github.com/jkoop
+        TXT . "\n");
+        chdir(__DIR__);
+
+        $exit_code = -1;
+        info("Building package: %s", $deb_file_name);
+        passthru("dpkg-deb --build $deb_file_name", $exit_code);
+        if ($exit_code != 0) {
+            error("dpkg-deb exit code non-zero: %d", $exit_code);
+            continue;
+        }
+
+        info("Cleaning up");
+        passthru('rm -rf tarball tarball.d ' . $deb_file_name);
+
+        $complete[] = $deb_file_name;
+        file_put_contents(COMPLETE_LIST_PATH, implode("\n", $complete));
+    }
+}
+passthru('rm -rf tarball tarball.d');
+
+// meta packages
+foreach ($releases as $version => $files) {
+    if ($version == "master") continue;
+    foreach ($files as $arch => $props) {
+        $package_date = date("Y-m-d");
+
+        if (!str_ends_with($arch, "-linux")) {
+            // debug("Arch not linux: %s", $arch);
+            continue;
+        }
+
+        // remove "-linux" from the end
+        $arch = substr($arch, 0, strpos($arch, "-linux"));
+
+        if (!isset(ARCH_MAP[$arch])) {
+            warning("Arch not supported: %s", $arch);
+            continue;
+        }
+
+        $arch = ARCH_MAP[$arch]; // convert to Debiany name
+        $minor_version = substr($version, 0, strrpos($version, "."));
+        $package_name = "zig";
+        $deb_file_name = $package_name . '_' . $version . '_' . $arch;
+
+        if (in_array($deb_file_name, $complete)) {
+            // info("Already packaged: %s", $deb_file_name);
+            continue;
+        }
+
+        passthru("rm -rf $deb_file_name $deb_file_name.deb");
+        printf("\n\tBuilding meta-package \"%s.deb\"\n\n", $deb_file_name);
+
+        info("Staging package: %s", $deb_file_name);
+        mkdir($deb_file_name . '/usr/sbin', recursive: true);
+        chdir($deb_file_name);
+        passthru('ln -s /usr/sbin/zig' . $minor_version . ' usr/sbin/' . $package_name);
+
+        $installed_size = trim(`du --block-size=1024 --summarize . | grep -Eo '^[0-9]*'`);
+        mkdir("DEBIAN");
+        file_put_contents("DEBIAN/control", <<<TXT
+        Package: $package_name
+        Version: $version
+        Architecture: $arch
+        Maintainer: Zig Software Foundation <https://ziglang.org/zsf>
+        Vendor: Joe Koop <https://github.com/jkoop>
+        Installed-Size: $installed_size
+        Depends: zig$minor_version (=$version)
+        Section: devel
+        Priority: optional
+        Homepage: https://ziglang.org/
+        Description: A programming language for robust and efficient software
+         Zig[1] is a general-purpose programming language and toolchain for
+         maintaining robust, optimal and reusable software.
+         .
+         (This is a meta-package that depends on zig$minor_version for version $version)
+         .
+         Built by ZSF on $files->date; packaged for Debian by Joe Koop[2] on $package_date
+         .
+         1: https://ziglang.org
+         2: https://github.com/jkoop
+        TXT . "\n");
+        chdir(__DIR__);
+
+        $exit_code = -1;
+        info("Building package: %s", $deb_file_name);
+        passthru("dpkg-deb --build $deb_file_name", $exit_code);
+        if ($exit_code != 0) {
+            error("dpkg-deb exit code non-zero: %d", $exit_code);
+            continue;
+        }
+
+        info("Cleaning up");
+        passthru('rm -rf ' . $deb_file_name);
+
+        $complete[] = $deb_file_name;
+        file_put_contents(COMPLETE_LIST_PATH, implode("\n", $complete));
+    }
+}
+
+info("DONE!");
+
+exit;
+
+function info(string $fmt, ...$values): void {
+    printf("INFO\t" . $fmt . "\n", ...$values);
+}
+
+function warning(string $fmt, ...$values): void {
+    printf("WARN\t" . $fmt . "\n", ...$values);
+}
+
+function error(string $fmt, ...$values): void {
+    printf("ERROR\t" . $fmt . "\n", ...$values);
+}
